6df3f744b7873d94476f2fb4ac7a2c3bbe650c75
Pedro Freisleben <pedrofreislebeen@gmail.com>
2024-01-22 02:00:04 -0300

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace JogoGourmet
+{
+    internal class Game
+    {
+        private static List<string> _availableDishes = new List<string> { "Lasanha", "Massa", "Bolo de Chocolate" };
+        public ReadOnlyCollection<string> AvailableDishes => _availableDishes.AsReadOnly();
+    }
+}
