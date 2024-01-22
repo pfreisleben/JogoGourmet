cef6cbf77b56ba2e91e07d6d3b96c9fea0a65dc6
Pedro Freisleben <pedrofreislebeen@gmail.com>
2024-01-22 02:05:03 -0300

    add functionality to game object

--- Game.cs
+++ Game.cs
@@ -6,5 +6,13 @@ namespace JogoGourmet
     {
         private static List<string> _availableDishes = new List<string> { "Lasanha", "Massa", "Bolo de Chocolate" };
         public ReadOnlyCollection<string> AvailableDishes => _availableDishes.AsReadOnly();
+        private int _numberOfHits = 0;
+        private int _numberOfMisses = 0;
+
+
+
+        private void IncrementNumberOfHits() => _numberOfHits++;
+        private void IncrementNumberOfMisses() => _numberOfMisses++;
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var game = new Game();
         }
     }
 }
